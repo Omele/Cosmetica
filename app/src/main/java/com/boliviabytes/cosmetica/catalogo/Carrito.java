eca02a841bb7db9c99000f45937fa35f5f17763a
Omele <nkaelth@gmail.com>
2015-11-29 08:06:50 -0400

    29-11-2015 Vista Principal y consume de Webservice 2--

---
+++ app/src/main/java/com/boliviabytes/cosmetica/catalogo/Carrito.java
@@ -0,0 +1,30 @@
+package com.boliviabytes.cosmetica.catalogo;
+
+/**
+ * @author HPIPP
+ * @version 1.0
+ * @created 28-Nov-2015 8:53:25 AM
+ */
+public class Carrito {
+
+	public Carrito(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void anhadirProducto(){
+
+	}
+
+	public ProductoPedido[] getProductoPedidos(){
+		return null;
+	}
+
+	public void quitarProducto(){
+
+	}
+
+}
---
+++ app/src/main/java/com/boliviabytes/cosmetica/catalogo/ProductoPedido.java
@@ -0,0 +1,16 @@
+package com.boliviabytes.cosmetica.catalogo;
+
+/**
+ * @author HPIPP
+ * @version 1.0
+ * @created 28-Nov-2015 8:56:03 AM
+ */
+public class ProductoPedido {
+
+	public ProductoPedido(){
+
+	}
+
+
+
+}
